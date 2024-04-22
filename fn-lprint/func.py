0feaa31ca1b5e89c34000d1911d47fd0c0257bea
Michael Mikhailidi <mikhailidim@gmail.com>
2024-04-22 18:40:37 -0400

    Inital FN version

--- fn-lprint/func.py
+++ fn-lprint/func.py
@@ -1,21 +1,39 @@
 import io
 import json
 import logging
+import socket
 
-from fdk import response
+#from fdk import response
 
-
-def handler(ctx, data: io.BytesIO = None):
-    name = "World"
+def handler(ctx, data: io.BytesIO=None):
+    payload = None
+    device = "localhost:9100"
+    enc = "base64"
+    rsp = { "result": 0 }
+    timeout = 10
+    reply = None
     try:
-        body = json.loads(data.getvalue())
-        name = body.get("name")
+        body = json.loads(data)
+        device = body.get("device")
+        timeout = body.config.pop("timeout",default=10)
+        
+        
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((device.split(":")[0], 
+                        int((device.split(":")[1]))))
+            s.settimeout(timeout)
+            s.sendall(payload)
+            reply = s.recv(1024)
+            s.close()
+        rsp["success"] = {"status": "Ok","Sent": len(payload), "Received": int(reply)}
+        
     except (Exception, ValueError) as ex:
-        logging.getLogger().info('error parsing json payload: ' + str(ex))
-
-    logging.getLogger().info("Inside Python Hello World function")
-    return response.Response(
-        ctx, response_data=json.dumps(
-            {"message": "Hello {0}".format(name)}),
-        headers={"Content-Type": "application/json"}
-    )
+        print(str(ex))
+        rsp["result"] = 1
+        rsp["error"] = { "message": str(ex)}
+        pass
+    return json.dumps(rsp)           
+    
+if __name__ == "__main__":
+    handler(None,json.dumps({"device" : "localhost:9100","payload": "SGVsbG8gV29ybGQhCg==" }))
+    
