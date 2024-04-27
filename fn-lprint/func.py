38bb6c270cc3cd131504f7a27ee04ea48fd258fb
Michael Mikhailidi <mikhailidim@gmail.com>
2024-04-27 11:55:42 +0000

    Logging and input validation

--- fn-lprint/func.py
+++ fn-lprint/func.py
@@ -11,26 +11,31 @@ def handler(ctx, data: io.BytesIO=None):
     timeout = 10
     reply = None
     lgr = logging.getLogger()
+    lgr.info("FN started. Perfroming function configuration.")
     try:
         # Retrieve key OCID and cryptographic endpoint
         cfg = ctx.Config()
+        lgr.info(f'FN Configuration received {cfg}')
+        # Default device from configuration
         device = cfg.get("device","localhost:9100")
-        lgr.setLevel(
-                    logging.getLevelName(
-                    cfg.get("log_level","INFO"))
-        )
+        lgr.setLevel(cfg.get("log_level","INFO"))
+        lgr.info(f'FN Configuration completed')
         
     except Exception as ex:
-        print('ERROR: Missing configuration key', ex, flush=True)
+        lgr.error(f'Missing configuration key {ex}', flush=True)
         raise    
     
     try:
         body = json.loads(data.getvalue())
-        lgr.debug(str(body))
-        lgr.debug(ctx)
-        device = body.get("device")
-        timeout = body.config.pop("timeout",default=10)
+        lgr.debug(f'Reciveid request {body}')
+        lgr.debug(f'Function context {ctx}')
+        # Test if device is requested
+
+        device = body.device if "device" in body else device
+        timeout = int(body.timout)  if "timeout" in body else timeout
+        lgr.debug(f"Configuration extraxted. Device {device}, timeout: {timeout}")
         payload = body.get("text")
+        lgr.info("Connecting to device")
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((device.split(":")[0], 
                         int((device.split(":")[1]))))
@@ -38,13 +43,14 @@ def handler(ctx, data: io.BytesIO=None):
             s.sendall(payload)
             reply = s.recv(1024)
             s.close()
-        rsp["success"] = {"status": "Ok","Sent": len(payload), "Received": int(reply)}        
+        rsp["success"] = {"status": "Ok","Sent": len(payload), "Received": int(reply)}    
+        lgr.debug(f"Ready to reply {rsp}")    
     except (Exception, ValueError) as ex:
-        print(str(ex))
+        lgr.error(f"Received exception {ex}")
         rsp["result"] = 1
         rsp["error"] = { "message": str(ex)}
         pass
-    
+    lgr.info(f"Function completed.")
     return response.Response(ctx, 
                             response_data=json.dumps(rsp),
                             headers={"Content-Type": "application/json"})
