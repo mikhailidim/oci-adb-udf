ad0705cf448d80a612e0cbb776080496208aac0c
Michael M <mikhailidim@users.noreply.github.com>
2024-05-12 09:12:16 -0400

    Ocifunc (#2)
    * Inital FN version
    
    * Return Response
    
    * Use configuration parameters
    
    * Log event
    
    * Console updates
    
    * Fix logLevel error
    
    * bytsio to value
    
    * Logging and input validation
    
    * Expanding log entries
    
    * Working basic code.
    
    * sync code
    
    * Process configuration
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
 ## Oracle Cloud Infrastructure Functions
 
-This repository contains sample code for presentation "OCI Functions: Key to database in Clouds".
+This repository contains sample code for the presentation "OCI Functions: Key to Database in Clouds."
 
-The repoistory conttains two projects:
+The repository contains two projects:
 
-* On premises device emulator. Detail descriptions are [here](./onprem-ascii-device/README.md).
-* Oracle OCI Function project. subfolder contains artefacts to build and deploy FDK compatible [code](./fn-lprint/README.MD).
+* On-premises device emulator. Detail descriptions are [here](./onprem-ascii-device/README.md).
+* Oracle OCI Function project. The subfolder contains artifacts for building and deploying FDK-compatible [code](./fn-lprint/README.MD).
 
+### Architecture Overview
 
+The code in the repository is compatible with multiple clouds or could be run on-premises. 
+However, the primary goal is to minimize Oracle Database code alterations during the cloud migration.
+The diagram below depicts the Oracle Autonomous Database Serverless integration with an "on-premises" print device.
+On the diagram:
+
+* Oracle Autonomous Database (ADB) provisioned managed service with VCN-only access.
+* Function and Printer images are stored in the Oracle Container Registry.
+* The printer emulator runs as a single OCI Container Instance with a printer port 9100.
+* OCI Function runs in the same compartment with access to the subnet.
+* Function presented as a user-defined database function and available as a regular SQL function.  
+
+![image](https://github.com/mikhailidim/oci-adb-udf/assets/10143072/6c92007d-36d7-4100-8606-a04f5fcb032c)
+
+Database code could call the function to send text and basic printer commands to the device using the local IP address and any random port. 
+That removes Oracle ADB limitations on accessing network or file resources. 
--- fn-lprint/func.py
+++ fn-lprint/func.py
@@ -1,21 +1,84 @@
 import io
 import json
 import logging
-
+import socket
 from fdk import response
 
+_PGL_STATUS = "@PJL INFO STATUS"
+_PGL_CONFIG = "@PJL CONFIG"
+lgr = logging.getLogger()
+lgr.info("FN started. Perfroming function configuration.")
 
-def handler(ctx, data: io.BytesIO = None):
-    name = "World"
+def commandProcessor(soc, cfg):
+    rpl = ""
+    for key,value in cfg:
+        soc.sendall(f"{_PGL_CONFIG} {key}={value}")
+        lgr.debug("Command was send")
+        rpl+" "+soc.recv(1024)       
+    return rpl
+
+def printText(soc, text):
+    rpl=""
+    soc.sendall(f"{_PGL_CONFIG} {key}={value}")
+    lgr.debug("Text was printed.")
+    rpl+" "+soc.recv(1024)       
+    return rpl;
+
+def handler(ctx, data: io.BytesIO=None):
+    payload = None
+    device = ""
+    rsp = { "result": 0 }
+    timeout = 10
+    reply = None
+    try:
+        # Retrieve key OCID and cryptographic endpoint
+        cfg = ctx.Config()
+        lgr.info(f'FN Configuration received {cfg}')
+        # Default device from configuration
+        device = cfg.get("device","localhost:9100")
+        lgr.setLevel(cfg.get("log_level","INFO"))
+        lgr.info(f'FN Configuration completed')
+        
+    except Exception as ex:
+        lgr.error(f'Missing configuration key {ex}', flush=True)
+        raise    
+    
     try:
         body = json.loads(data.getvalue())
-        name = body.get("name")
-    except (Exception, ValueError) as ex:
-        logging.getLogger().info('error parsing json payload: ' + str(ex))
+        lgr.debug(f'Reciveid request {body}')
+        lgr.debug(f'Function context {ctx}')
 
-    logging.getLogger().info("Inside Python Hello World function")
-    return response.Response(
-        ctx, response_data=json.dumps(
-            {"message": "Hello {0}".format(name)}),
-        headers={"Content-Type": "application/json"}
-    )
+        device = body["device"] if "device" in body else device
+        payload = body.get("text").encode()
+        lpconfig = body["config"] if "config" in body else device         
+        timeout = int(lpconfig["timeout"])  if "timeout" in lpconfig else timeout
+
+        lgr.debug(f"Configuration extraxted. Device {device}, timeout: {timeout}")
+        lgr.info("Connecting to device")
+        
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            addr = (device.split(":")[0], 
+                        int(device.split(":")[1]))
+            lgr.debug(f"Ready to access {addr}")            
+            s.connect(addr)
+            lgr.debug(f"Connected to {addr}")            
+            s.settimeout(timeout)            
+            lgr.debug("Timout adjusted.")
+            commandProcessor(s,lpconfig)
+            lgr.debug("Configuration processed.")
+            reply = printText(s,payload)
+        lgr.debug("Connection closed")
+        
+        rsp["success"] = {"status": "Ok","Sent": len(payload), "Received": len(reply)}    
+        lgr.debug(f"Ready to reply {rsp}")    
+        
+    except (Exception, ValueError) as ex:
+        lgr.error(f"Received exception {ex}")
+        rsp["result"] = 1
+        rsp["error"] = { "message": str(ex)}
+        pass
+    lgr.info(f"Function completed.")
+    
+    return response.Response(ctx, 
+                            response_data=json.dumps(rsp),
+                            headers={"Content-Type": "application/json"})
